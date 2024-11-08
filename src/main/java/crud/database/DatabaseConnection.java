b61c89975b07fcb0bdd8fc7704abf471ce6163ed
jawed <jaywed@hptmail.fr>
2024-11-08 11:03:54 +0100

    Ajout du .env

--- src/main/java/crud/database/DatabaseConnection.java
+++ src/main/java/crud/database/DatabaseConnection.java
@@ -1,13 +1,19 @@
 package crud.database;
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/crud_java";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String URL = dotenv.get("PATH_SQL");
+    private static final String USER = dotenv.get("USER_SQL");
+    private static final String PASSWORD = dotenv.get("PASSWORD_USER_SQL");
+
+    /*private static final String URL = "jdbc:mysql://localhost:3306/crud_java";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "";*/
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
